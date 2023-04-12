492f78555ded0bc064e9620c7e581b9ef4e9b7ad
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-04-12 12:18:35 -0700

    Going to finish when I return, finished 1st part

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,33 +1,166 @@
 //STEP 1
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// console.log(movies[1]);
 
 //STEP 2
+// let movies = new Array(5);
+
+// movies[0] = 'Hot Fuzz';
+// movies[1] = 'Everything Everywhere';
+// movies[2] = 'Dungeons and Dragons';
+// movies[3] = 'Knives Out';
+// movies[4] = 'John Wick';
+
+// console.log(movies[0]);
 
 //STEP 3
 
+// movies.splice(2, 0, "Cocaine Bear");
+
+// console.log(movies.length);
+
 //STEP 4
 
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+
+// delete movies[0];
+
+// console.log(movies);
+
 //STEP 5
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// for (let movie in movies) {
+//     console.log(movies[movie])
+// }
 
 //STEP 6
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// for (let i of movies) {
+//     console.log(i)
+// }
 
 //STEP 7
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// movies.sort();
+// console.log(movies);
 
 //STEP 8
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// let leastFavMovies = ['Morbius', 'The Room', 'Troll 2'];
+
+// console.log("Movies I like:  \n\n" + movies[0] + "\n" + movies[1] + "\n" + movies[2] + "\n\nMovies I don't like: \n\n" + leastFavMovies[0] + "\n" + leastFavMovies[1] + "\n" + leastFavMovies[2])
 
 //STEP 9
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// let leastFavMovies = ['Morbius', 'The Room', 'Troll 2'];
+
+// let Movies = movies.concat(leastFavMovies);
+
+// console.log(Movies.reverse());
 
 //STEP 10
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// let leastFavMovies = ['Morbius', 'The Room', 'Troll 2'];
+
+// let Movies = movies.concat(leastFavMovies);
+
+// console.log(Movies.slice(-1));
 
 //STEP 11
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// let leastFavMovies = ['Morbius', 'The Room', 'Troll 2'];
+
+// let Movies = movies.concat(leastFavMovies);
+
+// console.log(Movies[0]);
 
 //STEP 12
+// let movies = ['Hot Fuzz', 'Everything Everywhere', 'Dungeons and Dragons', 'Knives Out', 'John Wick'];
+// movies[5] = 'Cocaine Bear';
+// movies[6] = 'Nope';
+
+// let leastFavMovies = ['Morbius', 'The Room', 'Troll 2'];
+
+// let Movies = movies.concat(leastFavMovies);
+
+// let leastFavMoviesIndices = leastFavMovies.map(movie => Movies.indexOf(movie));
+
+// leastFavMoviesIndices.forEach(index => {
+//     Movies.splice(index, 0, "Shrek")
+// })
+
+// console.log(Movies);
 
 //STEP 13
+// let movies = [["HotFuzz", 1], ["Everything Everywhere", 2], ["Dungeons and Dragons", 3], ["Knives Out", 4], ["John Wick", 5]];
+// let movieText = movies.filter(movie => movie[0]).forEach(movie => console.log(movie[0]));
 
 //STEP 14
+// let employees = ["Zak", "Jessica", "Mark", "Fred", "Sally"];
+// let showEmployee = (function (emp) {
+//     console.log("Employees: \n\n" + emp.join("\n"))
+// });
+
+// showEmployee(employees);
 
 //STEP 15
 
+// let myArrayItems = [false, null, 0, '', 'hello', 42]
+// let result = myArrayItems.filter(checkType);
+
+// function checkType(myArrayItem) {
+//     if(myArrayItem !== false || myArrayItem !== null || myArrayItem !== 0 || myArrayItem !== "") {
+//         return myArrayItem
+//     }
+// };
+
+// console.log(result);
+
 //STEP 16
+// function getRandomItemFromArray(arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+//   }
+  
+
+// let numericArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let randomItem = getRandomItemFromArray(numericArray);
+// console.log(randomItem); 
+  
 
 //STEP 17
+// function getLargestNumber(arr) {
+//     let largestNumber = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > largestNumber) {
+//         largestNumber = arr[i];
+//       }
+//     }
+//     return largestNumber;
+//   }
+  
+ 
+// let numericArray = [4, 8, 2, 9, 3, 7, 1, 6, 5];
+// let largestNumber = getLargestNumber(numericArray);
+// console.log(largestNumber); 
+  
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,5 +1,9 @@
 // CREATE AN ARRAY OF EMPLOYEES
-
+let employees = [{id: 1, name:'John Doe', extension: 1234, email: 'john.doe@example.com', department: 'Sales'},
+    {id: 2, name: 'Jane Smith', extension: 5678, email: 'jane.smith@example.com', department: 'Marketing'},
+    {id: 3, name: 'Bob Johnson', extension: 9012, email: 'bob.johnson@example.com', department: 'Finance'},
+    {id: 4, name: 'Sarah lee', extension: 3456, email: 'sara.lee@example.com', department: 'HR'},
+    {id: 5, name: 'Mike Smith', extension: 7890, email: 'mike.smith@example.com', department: 'IT'}];
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
@@ -9,12 +13,14 @@
 
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
+window.addEventListener('load', () => {
+    buildGrid(employees);
+});
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault();
     // GET THE VALUES FROM THE TEXT BOXES
 
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
@@ -32,13 +38,15 @@ form.addEventListener('submit', (e) => {
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
     // CONFIRM THE DELETE
-
+    if (e.target.classList.contains("delete")) {
+        if (confirm('Are you sure you want to delete this employee?')) {
         // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
 
         // REMOVE EMPLOYEE FROM ARRAY
 
         // BUILD THE GRID
-
+        }
+    }
 });
 
 // BUILD THE EMPLOYEES GRID
